16fdf9a7b6708443130a4f7b2e00d4964f1223d4
MeutiaKhanandiya <khanandyam@gmail.com>
2021-03-12 21:55:47 +0700

    Menghubungkan Framework Flask ke template adminlte

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello! this is the main page <h1>HELLO<h1>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug= True)
